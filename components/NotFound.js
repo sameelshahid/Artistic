0eaac53f5c9cf317394055d08e0e2f059336bcca
sameelshahid <51823751+sameelshahid@users.noreply.github.com>
2022-06-26 16:46:54 +0500

    Displaying events with artists

--- components/NotFound.js
+++ components/NotFound.js
@@ -4,9 +4,6 @@ import Head from "next/head";
 const NotFound = () => {
   return (
     <>
-      <Head>
-        <title>Not Found</title>
-      </Head>
       <h4>Artist Not Found</h4>
     </>
   );
